00c07e649200a54e51c4c073b3de4df3ec83678f
Adelice <nancytoniw@gmail.com>
2019-02-19 01:43:48 +0200

    add static folder

--- app/request.py
+++ app/request.py
@@ -1,10 +1,10 @@
 from app import app
 import urllib.request,json
 from .models import source
-
 Source = source.Source
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+base_url = app.config["SOURCE_API_BASE_URL"]
 def get_sources(category):
     '''
     Function that gets the json response to our url request
@@ -15,11 +15,57 @@ def get_sources(category):
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
 
-        source_results = None
-
-        if get_sources_response['results']:
-            source_results_list = get_sources_response['results']
-            source_results = process_results(source_results_list)
+        source_sources = None
 
 
-    return source_results
+        if get_sources_response['sources']:
+            source_sources_list = get_sources_response['sources']
+            source_sources = process_sources(source_sources_list)
+
+
+    return source_sources
+def process_sources(source_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    source_sources = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category= source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        if url:
+            source_object = Source(id,name,description,url,category,language,country)
+            source_sources.append(source_object)
+
+    return source_sources
+def get_source(id):
+    get_source_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_source_details_url) as url:
+        source_details_data = url.read()
+        source_details_response = json.loads(source_details_data)
+
+        source_object = None
+        if source_details_response:
+            id= source_details_response.get('id')
+            name = source_details_response.get('name')
+            description = source_details_response.get('description')
+            url = source_details_response.get('url')
+            category = source_details_response.get('category')
+            language = source_details_response.get('language')
+            country = source_details_response.get('country')
+
+            source_object = Source(id,name,description,url,category,language,country)
+
+    return source_object
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from .request import get_sources, get_source
 from app import app
 @app.route('/')
 def index():
@@ -6,14 +7,23 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    general_news = get_sources('general')
+    business_news = get_sources('business')
+    technology_news= get_sources('technology')
+    sports_news=get_sources('sports')
+
+
+   
 
     title = 'Home - Welcome to The best News Highlight Review Website Online'
-    return render_template('index.html', title = title)
-
+    return render_template('index.html', title = title, general=general_news, business=business_news,technology=technology_news,sports=sports_news)
 @app.route('/news/<int:news_id>')
-def news(news_id):
+def news(id):
 
     '''
     View news page function that returns the news details page and its data
     '''
-    return render_template('news.html',id = news_id)
+    source = get_source(id)
+    title = f'{source.title}'
+
+    return render_template('news.html',id = news_id, source=source)
